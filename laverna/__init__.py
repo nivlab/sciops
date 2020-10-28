4f7af7bd3c75739b4caa50cc3352d0e2791548ce
Sam Zorowitz <szorowi1@gmail.com>
2020-10-28 12:56:47 -0400

    progress: reorganization, regression results, manuscript updates

--- laverna/__init__.py
+++
@@ -1,6 +0,0 @@
-"""laverna: code for simulating careless/inattentive behavior"""
-
-__version__ = '0.1'
-
-from .ordinal import simulate_item_response
-from . import task
--- laverna/task/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Simulation of task behavior."""
-
-from ._reversal import (simulate_reversal_task, ReversalTask)
