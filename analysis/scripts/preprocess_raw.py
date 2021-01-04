42ab75c3cb41f78dbe2348133a98c6a74eccc81c
szorowi1 <szorowi1@gmail.com>
2021-01-04 13:09:11 -0500

    organization + cleanup

--- analysis/scripts/preprocess_raw.py
+++ analysis/scripts/preprocess_raw.py
@@ -2,9 +2,10 @@ import os, json
 import numpy as np
 from os.path import dirname, join
 from pandas import DataFrame, concat
-ROOT_DIR = dirname(os.path.realpath(__file__))
-MTURK_DIR = os.path.join(ROOT_DIR, 'mturk')
-PROLIFIC_DIR = os.path.join(ROOT_DIR, 'prolific')
+ROOT_DIR = dirname(dirname(os.path.realpath(__file__)))
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+MTURK_DIR = os.path.join(ROOT_DIR, 'raw', 'mturk')
+PROLIFIC_DIR = os.path.join(ROOT_DIR, 'raw', 'prolific')
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 ### Main loop.
@@ -16,8 +17,8 @@ SURVEYS = []
 DATA = []
 
 ## Locate files.
-files = [os.path.join(ROOT_DIR,'mturk',f) for f in os.listdir(join(ROOT_DIR, 'mturk')) if f.endswith('json')] +\
-        [os.path.join(ROOT_DIR,'prolific',f) for f in os.listdir(join(ROOT_DIR, 'prolific')) if f.endswith('json')]
+files = [os.path.join(MTURK_DIR,f) for f in os.listdir(MTURK_DIR) if f.endswith('json')] +\
+        [os.path.join(PROLIFIC_DIR,f) for f in os.listdir(PROLIFIC_DIR) if f.endswith('json')]
 files = sorted(files)
 
 for f in files:
@@ -81,11 +82,11 @@ for f in files:
         rt = np.copy(np.round(survey['rt'] * 1e-3, 3))
         
         ## Reformat responses.
-        survey = {f'{prefix}-{k}'.lower(): survey['responses'][k] for k in sorted(survey['responses'])}
+        survey = {f'{prefix}_{k}'.lower(): survey['responses'][k] for k in sorted(survey['responses'])}
     
         ## Update dictionary.
         dd.update(survey)
-        dd[f'{prefix}-rt'] = rt
+        dd[f'{prefix}_rt'] = rt
 
     ## Append.
     SURVEYS.append(dd)
@@ -118,24 +119,66 @@ METADATA = DataFrame(METADATA).sort_values(['platform','subject'])
 SURVEYS = DataFrame(SURVEYS).sort_values(['platform','subject'])
 DATA = concat(DATA).sort_values(['platform','subject','trial'])
 
-## Update non-overlapping columns.
-METADATA.insert(11, 'other-platform', np.where(METADATA['prolific'].isnull(), METADATA['mturk'], METADATA['prolific']))
-METADATA.insert(12, 'prev-complete', np.where(METADATA['prolific-this-study'].isnull(), METADATA['mturk-this-study'], METADATA['prolific-this-study']))
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+### Preprocess data.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+## Update overlapping columns.
+METADATA.insert(11, 'other_platform', np.where(METADATA['prolific'].isnull(), METADATA['mturk'], METADATA['prolific']))
+METADATA.insert(12, 'prev_complete', np.where(METADATA['prolific-this-study'].isnull(), METADATA['mturk-this-study'], METADATA['prolific-this-study']))
 METADATA = METADATA.drop(columns=['prolific','prolific-this-study','mturk','mturk-this-study'])
 
-## Update BIS/Bas.
-cols = ['bisbas-q01','bisbas-q02','bisbas-q03','bisbas-q04',
-        'bisbas-q05','bisbas-q06','bisbas-q07','bisbas-q08',
-        'bisbas-q09','bisbas-q10','bisbas-q11','bisbas-q12']
+## Update BIS/BAS (reverse scoring: strongly disagree = 0, strongly agree = 3).
+## https://local.psy.miami.edu/people/faculty/ccarver/availbale-self-report-instruments/bisbas-scales
+cols = ['bisbas_q01','bisbas_q02','bisbas_q03','bisbas_q04',
+        'bisbas_q05','bisbas_q06','bisbas_q07','bisbas_q08',
+        'bisbas_q09','bisbas_q10','bisbas_q11','bisbas_q12']
 SURVEYS[cols] = (3 - SURVEYS[cols].astype(int)) + 1
 
-## Update SHAPS.
-cols = ['shaps-q01','shaps-q02','shaps-q03','shaps-q04','shaps-q05',
-        'shaps-q06','shaps-q07','shaps-q08','shaps-q09','shaps-q10',
-        'shaps-q11','shaps-q12','shaps-q13','shaps-q14']
+## Update SHAPS (reverse scoring: strongly disagree = 0, strongly agree = 3).
+## https://datashare.nida.nih.gov/instrument/snaith-hamilton-pleasure-scale
+cols = ['shaps_q01','shaps_q02','shaps_q03','shaps_q04','shaps_q05',
+        'shaps_q06','shaps_q07','shaps_q08','shaps_q09','shaps_q10',
+        'shaps_q11','shaps_q12','shaps_q13','shaps_q14']
 SURVEYS[cols] = 3 - SURVEYS[cols].astype(int)
 
-## Save.
-METADATA.to_csv(os.path.join(ROOT_DIR, 'metadata.csv'), index=False)
-SURVEYS.to_csv(os.path.join(ROOT_DIR, 'surveys.csv'), index=False)
-DATA.to_csv(os.path.join(ROOT_DIR, 'data.csv'), index=False)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+### Assemble subscales.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+## Define subscales.
+subscales = {
+    '7u':    ['7u7d_q01','7u7d_q03','7u7d_q04','7u7d_q06','7u7d_q07','7u7d_q08','7u7d_q13'],
+    '7d':    ['7u7d_q02','7u7d_q05','7u7d_q09','7u7d_q10','7u7d_q11','7u7d_q12','7u7d_q14'],
+    'gad7':  ['gad7_q01','gad7_q02','gad7_q03','gad7_q04','gad7_q05','gad7_q06','gad7_q07'],
+    'pswq':  ['pswq_q01','pswq_q02','pswq_q03'],
+    'bis':   ['bisbas_q01','bisbas_q02','bisbas_q03','bisbas_q04'],
+    'bas_r': ['bisbas_q05','bisbas_q06','bisbas_q07','bisbas_q08'],
+    'bas_d': ['bisbas_q09','bisbas_q10','bisbas_q11','bisbas_q12'],
+    'shaps': ['shaps_q01','shaps_q02','shaps_q03','shaps_q04','shaps_q05',
+              'shaps_q06','shaps_q07','shaps_q08','shaps_q09','shaps_q10',
+              'shaps_q11','shaps_q12','shaps_q13','shaps_q14'],
+}
+
+## Assemble items.
+ITEMS = np.append(['platform','subject'], np.concatenate(list(subscales.values())))
+ITEMS = SURVEYS[ITEMS]
+
+## Update columns.
+rename = {v:'_'.join([k, v.split('_')[-1]]) for k, v in subscales.items() for v in v}
+ITEMS = ITEMS.rename(columns=rename)
+
+## Compute sum scores.
+SCORES = ITEMS.copy()
+for k in subscales.keys(): SCORES[k] = SCORES.filter(regex=k).astype(int).sum(axis=1)
+SCORES = SCORES[np.append(['platform','subject'], list(subscales.keys()))]
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+### Save data.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+METADATA.to_csv(os.path.join(DATA_DIR, 'metadata.csv'), index=False)
+SURVEYS.to_csv(os.path.join(DATA_DIR, 'surveys.csv'), index=False)
+ITEMS.to_csv(os.path.join(DATA_DIR, 'items.csv'), index=False)
+SCORES.to_csv(os.path.join(DATA_DIR, 'scores.csv'), index=False)
+DATA.to_csv(os.path.join(DATA_DIR , 'data.csv'), index=False)
