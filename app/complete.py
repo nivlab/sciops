7bea530d549c8c8f4e7c4333de8bcbabedd00c07
szorowi1 <szorowi1@gmail.com>
2022-02-11 11:52:51 -0500

    mturk-two-step (4) - update complete.py

--- app/complete.py
+++ app/complete.py
@@ -8,48 +8,21 @@ bp = Blueprint('complete', __name__)
 def complete():
     """Present completion screen to participant."""
 
+    ## Access query string.
+    query_info = request.args
+
+    ## Confirm all TurkPrime metadata present.
+    fields = ['workerId','assignmentId','hitId','a','tp_a','b','tp_b','c','tp_c']
+    all_fields = all([f in query_info for f in fields])
+
     ## Error-catching: screen for missing session.
     if not 'workerId' in session:
 
         ## Redirect participant to error (missing workerId).
         return redirect(url_for('error.error', errornum=1000))
 
-    ## Case 1: visit complete page without previous completion.
-    elif 'complete' not in session:
-
-        ## Flag experiment as complete.
-        session['ERROR'] = "1005: Visited complete page before completion."
-        session['complete'] = 'reject'
-        write_metadata(session, ['ERROR','complete','code_reject'], 'a')
-
-        ## Redirect participant with decoy code.
-        url = "https://app.prolific.co/submissions/complete?cc=" + session['code_reject']
-        return redirect(url)
-
-    ## Case 2: visit complete page with previous rejection.
-    elif session['complete'] == 'success':
-
-        ## Update metadata.
-        session['WARNING'] = "Revisited complete."
-        write_metadata(session, ['WARNING'], 'a')
-
-        ## Redirect participant with completion code.
-        url = "https://app.prolific.co/submissions/complete?cc=" + session['code_success']
-        return redirect(url)
-
-    ## Case 3: visit complete page with previous rejection.
-    elif session['complete'] == 'reject':
-
-        ## Update metadata.
-        session['WARNING'] = "Revisited complete."
-        write_metadata(session, ['WARNING'], 'a')
-
-        ## Redirect participant with decoy code.
-        url = "https://app.prolific.co/submissions/complete?cc=" + session['code_reject']
-        return redirect(url)
-
-    ## Case 4: visit complete page with previous error.
-    else:
+    ## Case 1: visit complete page with previous rejection.
+    elif session.get('complete') == 'reject':
 
         ## Update metadata.
         session['WARNING'] = "Revisited complete."
@@ -57,3 +30,30 @@ def complete():
 
         ## Redirect participant to error (unusual activity).
         return redirect(url_for('error.error', errornum=1005))
+
+    ## Case 2: visit complete page with previous error.
+    elif session.get('complete') == 'error':
+
+        ## Update metadata.
+        session['WARNING'] = "Revisited complete."
+        write_metadata(session, ['WARNING'], 'a')
+
+        ## Redirect participant to error (unusual activity).
+        return redirect(url_for('error.error', errornum=1005))
+
+    ## Case 3: visit complete page but missing metadata.
+    elif session.get('complete') == 'success' and not all_fields:
+
+        ## Update metadata.
+        session['WARNING'] = "Revisited complete."
+        write_metadata(session, ['WARNING'], 'a')
+
+        ## Redirect participant with complete metadata.
+        url = "/complete?workerId=%s&assignmentId=%s&hitId=%s&a=%s&tp_a=%s&b=%s&tp_b=%s&c=%s&tp_c=%s" %(session['workerId'], session['assignmentId'], session['hitId'], session['a'], session['tp_a'], session['b'], session['tp_b'], session['c'], session['tp_c'])
+        return redirect(url)
+
+    ## Case 4: all else.
+    else:
+
+        ## Redirect participant with completion code.
+        return render_template('complete.html')
